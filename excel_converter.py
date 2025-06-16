991a72e568ef8f8bd195b194c13861a056142893
Tweizh <ghnmqdtg@gmail.com>
2025-06-16 15:42:25 +0800

    Fix: Remove duplicate rows while converting DB from `xlsx` to `csv`

--- excel_converter.py
+++ excel_converter.py
@@ -4,6 +4,9 @@ import pandas as pd
 df = pd.read_excel("./DB/product_dataset.xlsx")
 # Update the column names to match the expected format
 df.columns = ['product_id', 'product_name', 'unit', 'currency']
+# Remove duplicate rows
+df = df.drop_duplicates()
+# Save to CSV
 df.to_csv("./python-scripts/shared/product_db.csv", index=False)
 
 print("Conversion complete!")
